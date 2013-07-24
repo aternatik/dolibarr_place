5c7f6716c275be81f2d1b56e04acc1619318dff9
jfefe <jfefe@aternatik.fr>
2013-07-24 15:16:48 +0200

    Add descriptor and module base structure

---
+++ admin/about.php
@@ -0,0 +1,90 @@
+<?php
+/* Module to manage locations, buildings, floors and rooms into Dolibarr ERP/CRM
+ * Copyright (C) 2013	Jean-François Ferry	<jfefe@aternatik.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 	\file		place/admin/about.php
+ * 	\ingroup	place
+ * 	\brief		This file is about page of place module
+ */
+// Dolibarr environment
+$res = @include("../../main.inc.php"); // From htdocs directory
+if (! $res) {
+	$res = @include("../../../main.inc.php"); // From "custom" directory
+}
+
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once "../lib/place.lib.php";
+
+dol_include_once('/place/lib/PHP_Markdown_1.0.1o/markdown.php');
+
+
+//require_once "../class/myclass.class.php";
+// Translations
+$langs->load("place@place");
+
+// Access control
+if (! $user->admin) {
+	accessforbidden();
+}
+
+// Parameters
+$action = GETPOST('action', 'alpha');
+
+/*
+ * Actions
+ */
+
+/*
+ * View
+ */
+$page_name = "PlaceAbout";
+llxHeader('', $langs->trans($page_name));
+
+// Subheader
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">'
+	. $langs->trans("BackToModuleList") . '</a>';
+print_fiche_titre($langs->trans($page_name), $linkback);
+
+// Configuration header
+$head = placeAdminPrepareHead();
+dol_fiche_head(
+	$head,
+	'about',
+	$langs->trans("Module110110Name"),
+	0,
+	'place@place'
+);
+
+// About page goes here
+echo $langs->trans("PlaceAboutPage");
+
+echo '<br>';
+
+$buffer = file_get_contents(dol_buildpath('/place/README.md', 0));
+echo Markdown($buffer);
+
+echo '<br>',
+'<a href="' . dol_buildpath('/place/COPYING', 1) . '">',
+'<img src="' . dol_buildpath('/place/img/gplv3.png', 1) . '"/>',
+'</a>';
+
+llxFooter();
+
+$db->close();
---
+++ lib/place.lib.php
@@ -0,0 +1,54 @@
+<?php
+/* Module to manage locations, buildings, floors and rooms into Dolibarr ERP/CRM
+ * Copyright (C) 2013	Jean-François Ferry	<jfefe@aternatik.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	\file		lib/place.lib.php
+ *	\ingroup	place
+ *	\brief		This file is library for place module
+ */
+
+function placeAdminPrepareHead()
+{
+	global $langs, $conf;
+
+	$langs->load("place@place");
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = dol_buildpath("/place/admin/admin_place.php", 1);
+	$head[$h][1] = $langs->trans("Settings");
+	$head[$h][2] = 'settings';
+	$h++;
+	$head[$h][0] = dol_buildpath("/place/admin/about.php", 1);
+	$head[$h][1] = $langs->trans("About");
+	$head[$h][2] = 'about';
+	$h++;
+
+	// Show more tabs from modules
+	// Entries must be declared in modules descriptor with line
+	//$this->tabs = array(
+	//	'entity:+tabname:Title:@place:/place/mypage.php?id=__ID__'
+	//); // to add new tab
+	//$this->tabs = array(
+	//	'entity:-tabname:Title:@place:/place/mypage.php?id=__ID__'
+	//); // to remove a tab
+	complete_head_from_modules($conf, $langs, $object, $head, $h, 'place');
+
+	return $head;
+}
