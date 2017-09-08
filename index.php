b3bd19f343e05154bf22ccaa883354bf2debc94a
jfefe <jfefe@aternatik.fr>
2017-09-08 18:11:43 +0200

    Code style

--- index.php
+++ index.php
@@ -24,10 +24,12 @@
 
 // Change this following line to use the correct relative path (../, ../../, etc)
 $res = 0;
-if(file_exists('../main.inc.php') {               // For root directory
+if(file_exists('../main.inc.php')) {
 	$res = include '../main.inc.php';
-} elseif (!$res && file_exists('../../main.inc.php') { // For "custom" directory
+	// For root directory
+} elseif (!$res && file_exists('../../main.inc.php')) {
     $res = include '../../main.inc.php';
+    // For "custom" directory
 }
 if (!$res) {
     die('Include of main fails');
